ec229d09589f43e1c56dc9067f6a787422b8c49e
Álex González <agonzalezro@gmail.com>
2012-06-06 18:20:56 +0200

    Removed funny functions :)

--- requests/api.py
+++ requests/api.py
@@ -1,7 +1,7 @@
 import json
-
 import requests
-from flask import Flask, request
+
+from flask import Flask, request, jsonify
 from oauth_hook import OAuthHook
 
 
@@ -19,33 +19,30 @@ def get_fon_url(url):
     return response.content.split('OK: ')[-1]
 
 
-def _json_encoder(message='Posted and JSON returned!'):
-    '''Very simple json encoder to use on the responses.'''
-    return json.dumps({'message': message})
+def post_to_twitter(url):
+    hook = OAuthHook(access_token, access_token_secret,
+                     consumer_key, consumer_secret)
+    client = requests.session(hooks={'pre_request': hook})
+    status = 'Pygrunn #5 example: {url}'.format(url=url)
+    response = client.post('http://api.twitter.com/1/statuses/update.json',
+                           {'status': status})
+    return response.content
 
 
-@app.route('/image', methods=['POST'])
+@app.route('/image', methods=['POST', 'GET'])
 def image():
     '''If `post_to_twitter` field is sent, the image will be posted on
     twitter using requests-oauth.'''
     message = None
 
     if request.form.get('post_to_twitter'):
-        hook = OAuthHook(access_token, access_token_secret,
-                         consumer_key, consumer_secret)
-        client = requests.session(hooks={'pre_request': hook})
-        status = 'Pygrunn #5 example: {url}'.format(
-                     url=get_fon_url(request.form.get('url'))
-                 )
-        response = client.post('http://api.twitter.com/1/statuses/update.json',
-                               {'status': status})
-        message = response.content
+        url = get_fon_url(request.form.get('url'))
+        message = post_to_twitter(url)
 
     if request.headers.get('Accept') == 'application/json':
-        encoder = _json_encoder
-    else:
-        encoder = lambda x='I am sure that this is not a JSON :)': x
-    return encoder(*[message] if message else [])
+        return jsonify(message=message or 'This is JSON')
+    return message or 'I am sure that this is not a JSON'
+
 
 if __name__ == '__main__':
     app.run()
